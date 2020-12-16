3fbe0eb502bc78ec20d5caba31d7f3757d68512f
jinyounglee <hs25823@naver.com>
2020-12-16 22:13:53 +0900

    adding in Sanity Studio

---
+++ src/client.js
@@ -0,0 +1,9 @@
+import sanityClient from '@sanity/client';
+
+export default sanityClient({
+  projectId: '69ywlzhz',
+  dataset: "production"
+})
+
+
+// sk9JMssGTMfc1dKROt3YVVtiA9u3frXcB9AfEKgeuXVzE8qdMiBNdtTjZ9FJwn2Yzblv8eWTNbVdNw986dnUcskgM4GJGvbggZcq2O50WkCeNRVGG9LdL0vXKhO4v5A9jSRj4hyAthSKEzx76lrMO0nwbEZX0GBO3tSdfLRl4GWYEeDBteKX
